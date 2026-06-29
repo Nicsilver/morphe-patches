d1ccae8dc396322d406134122e90fb13c6c3ae0c
0xrxL <127248639+0xrxL@users.noreply.github.com>
2026-06-29 12:33:56 +0200

    fix(YouTube - System share sheet): Support older app versions

--- patches/src/main/kotlin/app/morphe/patches/youtube/layout/sharesheet/Fingerprints.kt
+++ patches/src/main/kotlin/app/morphe/patches/youtube/layout/sharesheet/Fingerprints.kt
@@ -8,9 +8,11 @@
 package app.morphe.patches.youtube.layout.sharesheet
 
 import app.morphe.patcher.Fingerprint
-import app.morphe.patcher.InstructionLocation
+import app.morphe.patcher.InstructionLocation.MatchAfterImmediately
+import app.morphe.patcher.checkCast
+import app.morphe.patcher.fieldAccess
 import app.morphe.patcher.methodCall
-import app.morphe.patcher.string
+import app.morphe.patcher.opcode
 import com.android.tools.smali.dexlib2.AccessFlags
 import com.android.tools.smali.dexlib2.Opcode
 
@@ -20,12 +22,41 @@ internal object ShareSheetPanelContentInitializationFingerprint : Fingerprint(
     parameters = listOf("L", "Ljava/util/Map;"),
     filters = listOf(
         methodCall(
-            opcode = Opcode.INVOKE_VIRTUAL,
-            name = "getSupportFragmentManager"
+            opcode = Opcode.INVOKE_INTERFACE,
+            smali = "Ljava/util/Iterator;->next()Ljava/lang/Object;"
         ),
-        string(
-            "fullscreen_spinner_fragment",
-            location = InstructionLocation.MatchAfterWithin(2)
+        opcode(opcode = Opcode.MOVE_RESULT_OBJECT, location = MatchAfterImmediately()),
+        checkCast(type = "Landroid/content/pm/ResolveInfo;", location = MatchAfterImmediately()),
+        fieldAccess(
+            opcode = Opcode.IGET_OBJECT,
+            smali = "Landroid/content/pm/ResolveInfo;->activityInfo:Landroid/content/pm/ActivityInfo;",
+            location = MatchAfterImmediately(),
         ),
-    ),
+        fieldAccess(
+            opcode = Opcode.IGET_OBJECT,
+            smali = "Landroid/content/pm/ActivityInfo;->applicationInfo:Landroid/content/pm/ApplicationInfo;",
+            location = MatchAfterImmediately(),
+        ),
+        fieldAccess(
+            opcode = Opcode.IGET_OBJECT,
+            smali = "Landroid/content/pm/ApplicationInfo;->packageName:Ljava/lang/String;",
+            location = MatchAfterImmediately(),
+        ),
+        fieldAccess(
+            opcode = Opcode.IGET_OBJECT,
+            smali = "Landroid/content/pm/ResolveInfo;->activityInfo:Landroid/content/pm/ActivityInfo;",
+            location = MatchAfterImmediately(),
+        ),
+        fieldAccess(
+            opcode = Opcode.IGET_OBJECT,
+            smali = "Landroid/content/pm/ActivityInfo;->name:Ljava/lang/String;",
+            location = MatchAfterImmediately(),
+        ),
+        opcode(opcode = Opcode.IF_EQZ, location = MatchAfterImmediately()),
+        methodCall(
+            opcode = Opcode.INVOKE_STATIC,
+            smali = "Landroid/text/TextUtils;->isEmpty(Ljava/lang/CharSequence;)Z",
+            location = MatchAfterImmediately(),
+        )
+    )
 )
